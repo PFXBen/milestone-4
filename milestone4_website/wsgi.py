19e6abcba01df686c06ec4aeb136c449a7856a84
Ben Willis <cw003504@live.reading.ac.uk>
2023-06-28 22:51:39 +0100

    meh

--- milestone4_website/wsgi.py
+++ milestone4_website/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+from dotenv import load_dotenv
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'milestone4_website.settings')
+os.setenv('DJANGO_SETTINGS_MODULE', 'milestone4_website.settings')
 
 application = get_wsgi_application()
